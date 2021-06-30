0400d78fd3ef82778e4b375a48ca0b0c4c3ceacc
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-06-30 19:33:14 +0900

    Render basic form #2

--- src/components/SurveyForm/SurveyAnswer.tsx
+++ src/components/SurveyForm/SurveyAnswer.tsx
@@ -1,10 +1,137 @@
-import React from 'react';
-import { Answer } from '../../models';
+import React, { useEffect, useRef, useState } from 'react';
+import { Answer, ControlTypes } from '../../models';
+import { v4 as uuidv4 } from 'uuid';
 
 interface SurveyAnswerProps {
     record: Answer;
+    onChange?: (values: string[]) => void;
 }
 
-export const SurveyAnswer = ({ record }: SurveyAnswerProps) => {
-    return <div></div>;
+export const SurveyAnswer = ({ record, onChange }: SurveyAnswerProps) => {
+    const id = `${uuidv4()}-${record.id}`;
+    const inputs: ControlTypes[] = ['text', 'date'];
+
+    const [answerValues, setAnswerValues] = useState<string[]>(['']);
+    const [controlIds, setControlIds] = useState<string[]>([id]);
+
+    const handleChange =
+        (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+            let value = e.currentTarget.value;
+
+            console.info('onchange');
+
+            if (e.currentTarget.type === 'checkbox') {
+                value = e.currentTarget.checked ? value : '';
+            }
+
+            setAnswerValues((prevState) => {
+                const temp = [...prevState];
+                temp.splice(index, 1, value);
+
+                return temp;
+            });
+        };
+
+    const handleClickAddMoreAnswer = () => {
+        setAnswerValues((prevState) => [...prevState, '']);
+        setControlIds((prevState) => [
+            ...prevState,
+            `${id}-${prevState.length + 1}`,
+        ]);
+    };
+
+    const handleClickRemoveAnswer = (index: number) => () => {
+        console.info('handleClickRemoveAnswer', index);
+        setAnswerValues((prevState) => {
+            const temp = [...prevState];
+            temp.splice(index, 1);
+            return temp;
+        });
+        setControlIds((prevState) => {
+            const temp = [...prevState];
+            temp.splice(index, 1);
+            return temp;
+        });
+    };
+
+    useEffect(() => {
+        console.info(`${record.id}`, answerValues);
+        if (onChange) {
+            onChange(answerValues.filter(Boolean).map((x) => x));
+        }
+    }, [answerValues]);
+
+    return (
+        <div className="answer-container">
+            <div>
+                {record.controlType !== 'checkbox' && record.label && (
+                    <label htmlFor={id}>{record.label}</label>
+                )}
+            </div>
+            <div className={`answers`}>
+                {record.controlType === 'radio' && (
+                    <div>
+                        <label>
+                            <input
+                                type={record.controlType}
+                                name={id}
+                                value="yes"
+                                onChange={handleChange(0)}
+                            />{' '}
+                            Yes
+                        </label>
+                        <label>
+                            <input
+                                type={record.controlType}
+                                name={id}
+                                value="no"
+                                onChange={handleChange(0)}
+                            />{' '}
+                            No
+                        </label>
+                    </div>
+                )}
+                {record.controlType === 'checkbox' && (
+                    <label>
+                        <input
+                            type={record.controlType}
+                            onChange={handleChange(0)}
+                            value={record.label}
+                        />{' '}
+                        {record.label}
+                    </label>
+                )}
+                {inputs.find((x) => x === record.controlType) &&
+                    controlIds?.map((controlId, index) => {
+                        return (
+                            <div key={controlId}>
+                                <input
+                                    id={controlId}
+                                    name={controlId}
+                                    type={record.controlType}
+                                    placeholder={record.placeholder}
+                                    value={answerValues[index]}
+                                    onChange={handleChange(index)}
+                                />
+                                {index > 0 && (
+                                    <button
+                                        onClick={handleClickRemoveAnswer(index)}
+                                    >
+                                        Remove
+                                    </button>
+                                )}
+                            </div>
+                        );
+                    })}
+            </div>
+
+            {record.isMultiple && (
+                <div>
+                    <button onClick={handleClickAddMoreAnswer}>
+                        Add more answer
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 };
--- src/components/SurveyForm/SurveyQuestion.tsx
+++ src/components/SurveyForm/SurveyQuestion.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Question } from '../../models';
 import { SurveyAnswer } from './SurveyAnswer';
 
+import './style.css';
+
 interface SurveyQuestionProps {
     record: Question;
 }
@@ -10,9 +12,25 @@ export const SurveyQuestion = ({ record }: SurveyQuestionProps) => {
     return (
         <div>
             <h3>{record.value}</h3>
-            {record.answers.map((answer) => (
-                <SurveyAnswer key={answer.id} record={answer} />
-            ))}
+            <div className="is-flex is-flex-direction-row answers-container">
+                <div>
+                    <div className="help">Left pane</div>
+                    {record.answers
+                        .filter((x) => x.position === 'left')
+                        .map((answer) => (
+                            <SurveyAnswer key={answer.id} record={answer} />
+                        ))}
+                </div>
+                <div>
+                    <div className="help">Right pane</div>
+
+                    {record.answers
+                        .filter((x) => x.position === 'right')
+                        .map((answer) => (
+                            <SurveyAnswer key={answer.id} record={answer} />
+                        ))}
+                </div>
+            </div>
         </div>
     );
 };
